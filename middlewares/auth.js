af4cd06d10a8124f370e0e67709953c96bc7ff67
Kanapin Marat <kanapinmarat@yandex.ru>
2023-01-30 09:44:43 +0600

    add auth,central error handling and validation

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,12 +8,13 @@ const Unauthorized = require('../errors/Unauthorized');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   const coockieToken = req.cookies.jwt;
+  console.log(coockieToken);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new Unauthorized('Необходима авторизация');
   }
 
-  const token = authorization ? authorization.replace('Bearer ', '') : coockieToken;
+  const token = !authorization ? coockieToken : authorization.replace('Bearer ', '');
   let payload;
 
   try {
