0a0de47608685767fcbd4d675734e845b6826244
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-03 17:41:56 +0600

    use cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,47 +5,6 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
-// const auth = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   let payload;
-
-//   try {
-//     if (!authorization || !authorization.startsWith('Bearer ')) {
-//       next(new Unauthorized('Необходима авторизация'));
-//     }
-
-//     const token = authorization.replace('Bearer ', '');
-//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-//   } catch (err) {
-//     next(new Unauthorized('Необходима авторизация'));
-//   }
-
-//   req.user = payload;
-
-//   return next();
-// };
-
-// const auth = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   const cookiesJWT = req.cookies.jwt;
-//   let payload;
-
-//   try {
-//     if (!authorization || !authorization.startsWith('Bearer ')) {
-//       if (!cookiesJWT) { next(new Unauthorized('Необходима авторизация')); }
-//     }
-
-//     const token = !authorization ? cookiesJWT : authorization.replace('Bearer ', '');
-//     payload = jwt.verify(token, JWT_SECRET);
-//   } catch (err) {
-//     next(new Unauthorized('Необходима авторизация'));
-//   }
-
-//   req.user = payload;
-
-//   return next();
-// };
-
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   const JWT = req.cookies.jwt;
@@ -53,7 +12,7 @@ const auth = (req, res, next) => {
 
   try {
     if (!authorization || !authorization.startsWith('Bearer ')) {
-      if (!JWT) { throw next(new Unauthorized('Необходима авторизация')); }
+      if (!JWT) { return next(new Unauthorized('Необходима авторизация')); }
     }
     const token = authorization.replace('Bearer ', '');
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
