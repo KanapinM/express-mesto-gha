5754b740970376d1da9abea099373b5d181200e1
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-03 17:10:11 +0600

    use cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
-const { JWT_SECRET } = process.env;
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
@@ -49,15 +48,16 @@ const Unauthorized = require('../errors/Unauthorized');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
-  const cookiesJWT = req.cookies.jwt;
+  const JWT = req.cookies.jwt;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    if (!cookiesJWT) { throw next(new Unauthorized('Необходима авторизация')); }
+    if (!JWT) { throw next(new Unauthorized('Необходима авторизация')); }
   }
-  const token = !authorization ? cookiesJWT : authorization.replace('Bearer ', '');
+  // const token = !authorization ? JWT : authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new Unauthorized('Необходима авторизация'));
   }
