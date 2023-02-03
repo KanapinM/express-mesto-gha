5a1d1586a0aba8b6e50a6a4081bf24c1220d5003
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-03 16:31:15 +0600

    use cookies

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET } = process.env;
+
 const BadRequest = require('../errors/BadRequest');
 const Conflict = require('../errors/Conflict');
 const NotFoundError = require('../errors/NotFoundError');
@@ -53,10 +55,10 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: '7d',
       });
-      res
+      return res
         .cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
         .send({ token });
     })
