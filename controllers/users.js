131c0801f34f9d737e6897851b6c9cb0a7fa7a9a
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-04 15:53:20 +0600

    use cookies

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const BadRequest = require('../errors/BadRequest');
 const Conflict = require('../errors/Conflict');
@@ -55,7 +54,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
         .send({ isToken: token });
     })
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
@@ -15,8 +14,8 @@ const auth = (req, res, next) => {
     if (!authorization || !authorization.startsWith('Bearer ')) {
       if (!JWT) { return next(new Unauthorized('Необходима авторизация')); }
     }
-    const token = !authorization ? JWT : authorization.replace('Bearer ', '');
-    payload = jwt.verify(token, JWT_SECRET);
+    const token = authorization.replace('Bearer ', '');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new Unauthorized('Необходима авторизация'));
   }
