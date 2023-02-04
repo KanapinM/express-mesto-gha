d08e8ed0dfd4e0045f0e6ab070d5bb7c35d00280
Kanapin Marat <kanapinmarat@yandex.ru>
2023-02-04 20:28:18 +0600

    use cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,15 @@
 require('dotenv').config();
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
 const { JWT_SECRET } = process.env;
 
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
 const auth = (req, res, next) => {
-  // const { authorization } = req.headers;
   const JWT = req.cookies.jwt;
   let payload;
 
   try {
-    // if (!authorization || !authorization.startsWith('Bearer ')) {
-    //   if (!JWT) { return next(new Unauthorized('Необходима авторизация')); }
-    // }
-    // const token = !authorization ? JWT : authorization.replace('Bearer ', '');
     payload = jwt.verify(JWT, JWT_SECRET);
   } catch (err) {
     return next(new Unauthorized('Необходима авторизация'));
